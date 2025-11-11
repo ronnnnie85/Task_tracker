48b84e4f7ea18edd61c2de5396adf1e633d0aba9
MainUser <user@example.com>
2025-11-11 12:20:26 +0700

    добавил пагинацию

--- config/settings.py
+++ config/settings.py
@@ -74,6 +74,11 @@ TEMPLATES = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
+
 WSGI_APPLICATION = "config.wsgi.application"
 
 
