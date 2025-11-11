e2683c9dfe72246aa6e4f56f3d0b3e07e00dfeee
MainUser <user@example.com>
2025-11-11 12:40:12 +0700

    добавил документацию

--- config/urls.py
+++ config/urls.py
@@ -17,10 +17,28 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Task Tracker API",
+        default_version="v1",
+        description="API трекера задач сотрудников",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("tasks.urls", namespace="tasks")),
     path("users/", include("users.urls", namespace="users")),
+
+    # документация
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="swagger-ui"),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="redoc"),
 ]
